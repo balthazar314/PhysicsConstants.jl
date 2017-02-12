e74e8dde9faa5adf3d4c382d76caf1bfcf1f4744
balthazar314 <pstuc002.public@gmail.com>
2017-02-12 17:07:48 -0500

    added export expression

--- src/PhysicsConstants.jl
+++ src/PhysicsConstants.jl
@@ -3,6 +3,8 @@ using SIUnits
 
 # this is just what's useful to me right now
 module Thermo
+	export Boltzmans_Constant, Ideal_Gas_Constant
+
 	const Boltzmans_Constant = 1.3806485279E-23*Joule/Kelvin
 	const Kb = Boltzmans_Constant
   
